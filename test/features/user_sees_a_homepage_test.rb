d785f5f660b66973926a478f325832a05093bfb3
Tim Allen <tnelsallen3@gmail.com>
2019-01-10 12:14:41 -0700

    Adds error page test

--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -7,4 +7,11 @@ class HomepageTest < CapybaraTestCase
     assert page.has_content?("Welcome!")
     assert_equal 200, page.status_code
   end
+
+  def test_user_can_see_error_page
+    visit '/page_that_does_not_exist'
+
+    assert page.has_content?("Page not found.")
+    assert_equal 404, page.status_code
+  end
 end
